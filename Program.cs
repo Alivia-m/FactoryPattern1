334817c2e1def8d47e1096423b86d22ce52d63d6
Alivia-m <aliviagayle@icloud.com>
2021-06-30 11:32:16 -0500

    completed to-do list

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FactoryPatternExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the amount of tires for the vehicle you want to create");
+            int wheelCount = int.Parse(Console.ReadLine());
+
+            var vehicle = VehicleFactory.GetVehicle(wheelCount);
+            vehicle.Drive();
+        }
+    }
+}
---
+++ VehicleFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatternExercise
+{
+    public static class VehicleFactory
+    {
+        public static IVehicle GetVehicle(int wheelCount)
+        {
+            if(wheelCount == 4)
+            {
+                return new Car();
+            }
+            if(wheelCount == 2)
+            {
+                return new Motorcycle();
+            }
+            else
+            {
+                return new Car();
+            }
+        }
+    }
+}
